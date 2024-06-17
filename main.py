1947c6527c4d8765abfaaa4ffd3b21da88b693fc
raderth <50417647+raderth@users.noreply.github.com>
2024-06-17 22:58:44 +0100

    improvements

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import random
 
-length = {5:1,6:1,7:17,8:46,9:30,10:5} # This is the approximate distribution of place names' lengths
+length = {5:1,6:1,7:14,8:40,9:30,10:4,11:4,12:3,13:3}
 vowels = {'E': 12.02, 'A': 8.12, 'O': 7.68, 'I': 7.31, 'U': 2.88} # I pulled these numbers from the probability of a letter being in a word
 consonants = {'T': 9.10, 'N': 6.95, 'S': 6.28, 'R': 6.02, 'H': 5.92, 'D': 4.32, 'L': 3.98, 'C': 2.71, 'M': 2.61, 'F': 2.30, 'Y': 2.11, 'W': 2.09, 'G': 2.03, 'P': 1.82, 'B': 1.49, 'V': 1.11, 'K': 0.69, 'X': 0.17, 'Q': 0.11, 'J': 0.10, 'Z': 0.07}
 sum_length = sum(length[i] for i in length)
 sum_vowels = sum(vowels[i] for i in vowels)
 sum_cononants = sum(consonants[i] for i in consonants)
 
-combos_to_avoid_like_the_plague = ["OAH","BAO","AVI","IEG","TUI","OIR","EYI","IDG","UHO","RTV","IFT","IUH","NKA","AIY","PB","OGF","AEY","OAY","IOD","UOT","RWO","EAY","IOM","OBT","EHA","INP","UES","IEH","GSI","AVL","YAI","AUC","EUM","DIU","IRT","AIH","EOD","ATUH","AB","RIR","DOA","IWN","EYT","AED","EOR","EC","OEN","ASN","UED","BHE","II","IUG","IAS","OAG","IOR","AOP","OEV","IAH","IOY","EUTN","ERV","ICW","SNIO","YBE","YMI","AX","AAN","UU","UOF","HH","KWI","IUW","EIW","EIH","UDW","FDA","EOH","LW","APU","OIY","MHI","IOH","RYI","AOF","NN", "XK", "PT", "BV", "QW", "ZX", "VX", "VW", "VF", "GV", "ZV", "VV", "FV", "YH", "EHY", "HIH", "YC", "TG", "EEH", "WEE", "IGT", "IDF", "EIG", "PYT", "LH", "NC"]
+combos_to_avoid_like_the_plague = ["IAG","EDB","IDAO","OAH","BAO","AVI","IEG","TUI","OIR","EYI","IDG","UHO","RTV","IFT","IUH","NKA","AIY","PB","OGF","AEY","OAY","IOD","UOT","RWO","EAY","IOM","OBT","EHA","INP","UES","IEH","GSI","AVL","YAI","AUC","EUM","DIU","IRT","AIH","EOD","ATUH","AB","RIR","DOA","IWN","EYT","AED","EOR","EC","OEN","ASN","UED","BHE","II","IUG","IAS","OAG","IOR","AOP","OEV","IAH","IOY","EUTN","ERV","ICW","SNIO","YBE","YMI","AX","AAN","UU","UOF","HH","KWI","IUW","EIW","EIH","UDW","FDA","EOH","LW","APU","OIY","MHI","IOH","RYI","AOF","NN", "XK", "PT", "BV", "QW", "ZX", "VX", "VW", "VF", "GV", "ZV", "VV", "FV", "YH", "EHY", "HIH", "YC", "TG", "EEH", "WEE", "IGT", "IDF", "EIG", "PYT", "LH", "NC"]
 doubles_to_reduce = ["AA", "BB", "CC", "EE", "GG", "HH", "JJ", "KK", "PP", "QQ", "RR", "UU", "VV", "WW", "XX", "YY", "ZZ","TT"]
+combos_to_reduce_to_first = ["EI", "AE", "EAU", "OU", "AI", "OA", "AU", "IE", "IO", "EU", "EO", "IU", "AIY", "AY", "EAY", "EY", "OY", "IY", "UAI", "OAU","IAY", "OAY", "EAI", "EAO", "IAO", "IAE", "EOU", "UEA", "AOE", "AIE","OUE", "UEI", "UIE", "UIO", "EAI", "EIA", "AEI", "AIO", "AEO", "AOI","OIA", "OIE", "OUE", "IOE", "UIE", "UIO", "EIO", "EIA", "IUE", "IOU","AOI", "OAI", "OAE", "IAO", "IUE", "IOE", "UIE", "EIO", "EAI", "IUE","IAU", "UAI", "EIA", "IOA", "IEO", "UEO", "UEA", "AIE", "UAE", "UEO","OAE", "IEA", "OEU", "AUI", "IUA", "AUE", "IAE", "UAE", "AIE", "UAI","IEA", "UEO", "IAU", "UEI", "IAE", "UOE", "EAO", "OAI", "OEA", "IEA","UEI", "OEU", "IOA", "EIA", "UEA", "OIE", "AOI", "EIO"]
+combos_to_reduce_to_last = ["AO", "UIY", "OEG", "OU", "AI", "OE", "IE", "IO", "EA", "AU", "EU", "IU", "AIY", "OAY", "UE", "AE", "EI", "IA", "OY","EY", "IAO", "OIE", "IUE", "IEU", "OAU", "UEO", "IOE", "IEO", "OEA", "EIA", "AEO", "UEA", "OAE", "IAU", "EIO", "IOA", "OIA","IUA", "UAE", "UEI", "AUE", "OEU", "OIE", "EAI", "IAE", "OUI", "UEI", "OAI", "AIO", "OAE", "IEA", "EIA", "UIE", "UAI", "IEO", "UAE", "AIE", "IAU", "OIA", "EAO", "IAE", "UEA", "OAI", "UEO", "OEU", "UIO", "UIE", "EUI", "OUI", "UOE", "AOE", "IAO", "EIA","OAI", "IEA", "UEO", "AEO", "OEU", "IAE", "UEA", "IAU", "AIE","OIE", "UIE", "UAI", "EIA", "UEO", "AEO", "OIA", "IAU", "AIE","UEA", "OIE", "UIO", "AOE", "IEA"]
 
 word = ""
 
@@ -40,7 +42,7 @@ def find_word():
         vowel_last_time = vowel
     return(lword)
 
-for i in range(10):
+for i in range(100):
     repeat = True
     while repeat:
         repeat = False
@@ -50,7 +52,6 @@ for i in range(10):
             found = True
             for i in combos_to_avoid_like_the_plague:
                 if i in word:
-                    print("Bad Combo " + i + " - " + word)
                     if len(word) >= 7:
                         word = word.replace(i, "")
                     else:
@@ -62,12 +63,22 @@ for i in range(10):
             has_doubles = False
             for i in doubles_to_reduce:
                 if i in word:
-                    print("Double Letter " + i + " - " + word)
                     word = word.replace(i, i[0])
                     has_doubles = True
 
+        has_reductions = True
+        while has_reductions:
+            has_reductions = False
+            for i in combos_to_reduce_to_first:
+                if i in word:
+                    word = word.replace(i, i[0])
+                    has_reductions = True
+            for i in combos_to_reduce_to_last:
+                if i in word:
+                    word = word.replace(i, i[-1])
+                    has_reductions = True
+
         if len(word) < 5:
             repeat = True
-            print(word + " is unsalvagable")
 
     print(word)
